491978ac944f4d67c491ffb2257bf6e50cafe195
vishal <vishaltrycode@gmail.com>
2026-05-18 11:29:39 +0530

    Array List Added

---
+++ ForLoopDemo/src/com/java/controlstatement/ArrayList.java
@@ -0,0 +1,12 @@
+package com.java.controlstatement;
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+		java.util.ArrayList<Integer> ls=new java.util.ArrayList<Integer>();
+		ls.add(10);
+		ls.add(20);
+		
+		System.out.println(ls);
+	}
+}
